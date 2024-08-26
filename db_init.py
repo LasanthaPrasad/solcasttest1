2103e180ae424ce373cf3a75df8b7b7f66fac4dc
LasanthaPrasad <ee_prasad@yahoo.com>
2024-08-26 20:35:01 +0530

    first commit

--- db_init.py
+++ db_init.py
@@ -4,10 +4,12 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///solar_forecast.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
+
+
 
 def init_db():
     app = create_app()
